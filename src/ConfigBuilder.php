14821332667e4d4f546228fde7b3e13b249cdba3
Kaspars Dambis <hi@kaspars.net>
2020-02-20 15:54:09 +0200

    Produce a revision based prototype instead

--- src/ConfigBuilder.php
+++ src/ConfigBuilder.php
@@ -14,32 +14,43 @@ class ConfigBuilder
 
 		$repositories = array_map(
 			function ($plugin) use ($plugins_dir) {
-				return [
-					'type' => 'package',
-					'package' => [
-						'name' => 'xwp-vip-wpcom-plugins/' . $plugin,
-						'type' => 'wordpress-plugin',
-						'version' => 'dev-master',
-						'source' => [
-							'url' => 'https://vip-svn.wordpress.com/plugins',
-							'type' => 'svn',
-							'reference' => $plugin,
-						],
-						'dist' => [
-							'url' => sprintf('%s/%s', $plugins_dir, $plugin),
-							'type' => 'path',
-						],
-					],
-				];
+				$plugin_dir_path = sprintf( '%s/%s', $plugins_dir, $plugin );
+				$revisions = self::getDirectorySvnRevisions( $plugin_dir_path );
+
+				$packages = array_map(
+					function($revision) use ($plugin) {
+						return [
+							'type' => 'package',
+							'package' => [
+								'name' => 'xwp-vip-wpcom-plugins/' . $plugin,
+								'type' => 'wordpress-plugin',
+								'version' => sprintf('dev-r%s', $revision),
+								'source' => [
+									'url' => 'https://vip-svn.wordpress.com/plugins',
+									'type' => 'svn',
+									'reference' => sprintf('%s@%s', $plugin, $revision),
+								]
+							],
+						];
+					},
+					$revisions
+				);
+
+				return $packages;
 			},
-			array_map('basename', self::getDirectories($plugins_dir))
+			array_map('basename', self::getDirectories($plugins_dir) )
 		);
+		
+		$allRepositories = [];
+		foreach ($repositories as $packageRepos) {
+			$allRepositories = array_merge($allRepositories, $packageRepos);
+		}
 
 		$config = json_encode( [
 			'name' => 'xwp/vip-wpcom-plugins',
 			'homepage' => 'https://xwp.github.io/vip-wpcom-plugins',
 			'require-all' => true,
-			'repositories' => $repositories,
+			'repositories' => $allRepositories,
 			'archive' => [
 				'directory' => 'dist',
 				'format' => 'zip',
@@ -53,5 +64,17 @@ class ConfigBuilder
 		$pattern = sprintf('%s/*', rtrim($path, '/\\'));
 
 		return glob($pattern , GLOB_ONLYDIR);
+	}
+
+	protected static function getDirectorySvnRevisions($path) {
+		$logCommand = sprintf('svn log --xml %s', $path);
+		$xml = simplexml_load_string(shell_exec($logCommand));
+		$revisions = [];
+
+		foreach ($xml->logentry as $logentry) {
+			$revisions[] = (int) $logentry->attributes()->revision;
+		}
+
+		return $revisions;
 	}
 }
